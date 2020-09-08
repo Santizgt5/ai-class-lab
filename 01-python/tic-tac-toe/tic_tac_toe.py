b889c0bdacebc4a8b6c8e96ccdc713b1e9eb39ff
unknown <santiago.alvarez@imagineapps.co>
2020-09-08 01:38:37 -0500

    Juego tic tac toe realizado

--- 01-python/tic-tac-toe/tic_tac_toe.py
+++ 01-python/tic-tac-toe/tic_tac_toe.py
@@ -1,4 +1,6 @@
 import re
+import random
+import numpy as np
 
 _PLAYER = "player"
 _MACHINE = "machine"
@@ -12,9 +14,17 @@ class TicTacToeGame():
     self.turn = _PLAYER
     self.is_game_over = False
     self.winner = None
+    self.filaPlayer = ["x","x","x"]
+    self.filaMachine = ["o","o","o"]
 
   def is_over(self): # TODO: Finish this function by adding checks for a winning game (rows, columns, diagonals)
-    return self.board.count(None) == 0
+    if (self.board.count(None) != 0):
+      tableroArray = np.array(self.board)
+      tableroMatrix = tableroArray.reshape((3,3))   
+      return self.verificarGanadorHorizontal(tableroMatrix) or self.verificarGanadorVertical(tableroMatrix) or self.verificarGanadorDiagonal(tableroMatrix)
+    else:
+      self.print_result("empate")
+      return True
 
   def play(self):
     if self.turn == _PLAYER:
@@ -37,6 +47,7 @@ class TicTacToeGame():
 
     player_cell = int(player_cell)
 
+
     if self.board[player_cell] is not None:
       print("Cell is already taken, try again")
 
@@ -52,25 +63,81 @@ class TicTacToeGame():
   def machine_turn(self):
     # TODO: Implement this function to make the machine choose a random cell (use random module)
     # The result of this function should be that self.board now has one more random cell occupied
-
-    for i, cell in enumerate(self.board):
-      if cell is None:
-        self.board[i] = _MACHINE_SYMBOL
-        break
+    numero = random.randint(0, 8)
+    while(self.board[numero]):
+      numero = random.randint(0,8)
+    self.board[numero] = _MACHINE_SYMBOL
 
   def format_board(self):
-    # TODO: Implement this function, it must be able to print the board in the following format:
-    #  x|o| 
-    #   | | 
-    #   | | 
-    return self.board
+    tablero = ''
+    for x in range(0, len(self.board)):
+      c = str(self.board[x]) if self.board[x] else ' '
+      if (x+1) % 3 == 0:
+        tablero = tablero + ' ' + c +'\n'
+      else:
+        if x == 0:
+          tablero = ' '+ c + '   |   '
+        else:
+          tablero = tablero + ' ' +  c  + '   |   ' 
+    return tablero
 
   def print(self):
     print("Player turn:" if self.turn == _MACHINE else "Machine turn:")
     print(self.format_board())
     print()
 
-  def print_result(self):
-    # TODO: Implement this function in order to print the result based on the self.winner
-
+  def print_result(self, ganador):
+    if (ganador != "empate"):
+      print("El ganador fue "+ ganador)
+    else:
+      print("No hay ganadores, es un empate")
     pass
+
+  def verificarGanadorHorizontal(self, tableroMatrix):
+    cadena = str(np.array(self.filaPlayer))
+    cadena2 = str(np.array(self.filaMachine))
+    fila1 = str(tableroMatrix[0])
+    fila2 = str(tableroMatrix[1])
+    fila3 = str(tableroMatrix[2])
+    if cadena == fila1 or cadena == fila2 or cadena == fila3:
+      self.print_result("el jugador")
+      return True
+    elif cadena2 == fila1 or cadena2 == fila2 or cadena2 == fila3:
+      self.print_result("la maquina")
+      return True
+    else:
+      return False
+
+  def verificarGanadorVertical(self, tableroMatrix):
+    cols = tableroMatrix.transpose();
+    cadena = str(np.array(self.filaPlayer))
+    cadena2 = str(np.array(self.filaMachine))
+    fila1 = str(cols[0])
+    fila2 = str(cols[1])
+    fila3 = str(cols[2])
+    if cadena == fila1 or cadena == fila2 or cadena == fila3:
+      self.print_result("el jugador")
+      return True
+    elif cadena2 == fila1 or cadena2 == fila2 or cadena2 == fila3:
+      self.print_result("la maquina")
+      return True
+    else:
+      return False
+
+  def verificarGanadorDiagonal(self, tableroMatrix):
+    diagonal1 = [tableroMatrix[0][0],
+                     tableroMatrix[1][1],
+                     tableroMatrix[2][2]]
+
+    diagonal2 = [tableroMatrix[0][2],
+                     tableroMatrix[1][1],
+                     tableroMatrix[2][0]]
+    if diagonal1 == self.filaPlayer or diagonal2 == self.filaPlayer:
+      self.print_result("el jugador")
+      return True
+    elif diagonal1 == self.filaMachine or diagonal2 == self.filaMachine:
+      self.print_result("la maquina")
+      return True
+    else:
+      return False
+        
